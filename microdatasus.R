c1028b926af2d3f329f5f80dffc1e96ac3e5febb
Vinicius Wanis Correa Santos <waniscorreas@gmail.com>
2025-03-20 18:56:31 -0300

    Instalação Microdatasus.R

---
+++ microdatasus.R
@@ -0,0 +1,10 @@
+## Acesso ao Microdata SUS ##
+
+#install.packages("remotes")
+#library(remotes)
+
+#remotes::install_github("rfsaldanha/microdatasus")
+
+## Realizado as devidas instalações dos pacotes ##
+
+library(microdatasus)
